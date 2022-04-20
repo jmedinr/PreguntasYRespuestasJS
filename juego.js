22a5dcb21212d09cd1ffa9b338cb356327d6d650
pilar <ingeniera.coral@gmail.com>
2022-04-20 16:32:00 -0500

    se crea modelo vista controlador

---
+++ juego.js
@@ -0,0 +1,83 @@
+// modelo: aqui van solo datos, no funciones
+
+function view_iniciar_juego() {
+  document.getElementById("root").innerHTML = `
+    <h1>Bienvenido al juego de preguntas</h1>
+    <h3>Vamos a jugar:</h3> 
+    <input type="text" placeholder="NICKNAME"name="nickname" id="nickname">
+    
+    <h5>este juegoo se trata de blablabla</h5>
+   
+    <br>
+    <br>
+    <button onclick="ctrl_iniciarPartida()">Iniciar partida</button>
+    <br>
+    <br>
+    
+    <button onclick="ctrl_irAMenuPrincipal()">Regresar a menu principal</button>
+    `;
+}
+
+// controladores: solo ellos pueden cambiar el modelo y las vistas
+
+function view_iniciarPartida() {
+  document.getElementById("root").innerHTML = `
+  
+  <section id="pregunta">${
+    modelo.preguntas[modelo.preguntaActual].pregunta.pregunta
+  }</section>
+  <section id="resp1" class="respuesta rizq" onclick="ctrl_dioClickEnRespuesta(1)">A)<span id="R1">${
+    modelo.preguntas[modelo.preguntaActual].pregunta.opcion1
+  }</span></section>
+	<section id="resp2" class="respuesta rder" onclick="ctrl_dioClickEnRespuesta(2)">B)<span id="R2">${
+    modelo.preguntas[modelo.preguntaActual].pregunta.opcion2
+  }</span></section>
+	<section id="resp3" class="respuesta rizq" onclick="ctrl_dioClickEnRespuesta(3)">C)<span id="R3">${
+    modelo.preguntas[modelo.preguntaActual].pregunta.opcion3
+  }</span></section>
+	<section id="resp4" class="respuesta rder" onclick="ctrl_dioClickEnRespuesta(4)">D)<span id="R4">${
+    modelo.preguntas[modelo.preguntaActual].pregunta.opcionCorrecta
+  }</span></section>
+	<button id="iniciar" class="panel_sup" onclick="ctrl_irAMenuPrincipal();" >Salir</button>
+	<div id="wrap_premio" class="panel_sup">$<span id="premio">${
+    modelo.acumulado
+  }</span></div>
+  
+    `;
+}
+
+function view_partidaPerdida() {
+  document.getElementById("root").innerHTML = `
+    <h3>Perdiste:</h3> 
+    <button onclick="ctrl_iniciarPartida()">Iniciar nueva partida</button>
+    <br>
+    <br>
+    
+    <button onclick="ctrl_irAMenuPrincipal()">Regresar a menu principal</button>
+    `;
+}
+
+function ctrl_iniciarPartida() {
+  modelo.preguntas.sort(() => Math.random() - 0.5);
+  view_iniciarPartida();
+}
+
+function ctrl_irAMenuPrincipal() {
+  view_menuPrincipal();
+}
+
+function ctrl_partidaPerdida() {
+  view_menuPrincipal();
+}
+
+function ctrl_dioClickEnRespuesta(respuesta) {
+  if (respuesta === 4) {
+    console.log("correcto");
+    modelo.preguntaActual += 1;
+    modelo.acumulado += 100;
+    view_iniciarPartida();
+  } else {
+    console.log(respuesta);
+    view_partidaPerdida();
+  }
+}
---
+++ modelo.js
@@ -0,0 +1,154 @@
+modelo = {
+  preguntas: [
+    {
+      pregunta: {
+        categoria: "literatura",
+        nivel: "2",
+        pregunta:
+          "¿Cuál es el libro más vendido en el mundo después de la Biblia?",
+        opcion1: "La Odisea",
+        opcion2: "El Señor de los Anillos",
+        opcion3: "Cien años de Soledad",
+        opcionCorrecta: "Don Quijote de la Mancha",
+      },
+    },
+    {
+      pregunta: {
+        categoria: "quimica",
+        nivel: "1",
+        pregunta: "La sal común está formada por dos elementos, ¿cuáles son?",
+        opcion1: "Sodio y potasio",
+        opcion2: "Sodio y carbono",
+        opcion3: "Cristal y sodio",
+        opcionCorrecta: "Sodio y cloro",
+      },
+    },
+    {
+      pregunta: {
+        categoria: "historia",
+        nivel: "4",
+        pregunta: "¿Quién pintó la obra Guernica?",
+        opcion1: "Leonardo DaVinci",
+        opcion2: "Frida Kahlo",
+        opcion3: "Diego Rivera",
+        opcionCorrecta: "Pablo Picasso",
+      },
+    },
+    {
+      pregunta: {
+        categoria: "historia",
+        nivel: "3",
+        pregunta:
+          "¿Cuál es el tema del famoso discurso -I have a dream- de Martin Luther King?",
+        opcion1: "La Justicia para los menos favorecidos",
+        opcion2: "La intolerancia religiosa",
+        opcion3: "El premio Nobel de la Paz",
+        opcionCorrecta: "La igualdad de las razas",
+      },
+    },
+    {
+      pregunta: {
+        categoria: "historia",
+        nivel: "4",
+        pregunta: "¿En qué periodo de la prehistoria fue descubierto el fuego?",
+        opcion1: "Neolítico",
+        opcion2: "Edad de los metales",
+        opcion3: "Edad de piedra",
+        opcionCorrecta: "Paleolítico",
+      },
+    },
+    {
+      pregunta: {
+        categoria: "biologia",
+        nivel: "3",
+        pregunta: "¿Cuál es el cromosoma que determina el sexo masculino?",
+        opcion1: "El cromosoma XX",
+        opcion2: "El cromosoma X",
+        opcion3: "El cromosoma M",
+        opcionCorrecta: "El cromosoma Y",
+      },
+    },
+    {
+      pregunta: {
+        categoria: "quimica",
+        nivel: "4",
+        pregunta: "¿Cuál es el metal cuyo símbolo químico es Au?",
+        opcion1: "Aluminio",
+        opcion2: "Arsénico",
+        opcion3: "Hierro",
+        opcionCorrecta: "Oro",
+      },
+    },
+    {
+      pregunta: {
+        categoria: "historia",
+        nivel: "5",
+        pregunta: "¿Quién pintó la bóveda de la capilla sixtina?",
+        opcion1: "Donatello",
+        opcion2: "Boticelli",
+        opcion3: "Leonardo da Vinci",
+        opcionCorrecta: "Miguel Angel",
+      },
+    },
+    {
+      pregunta: {
+        categoria: "zoologia",
+        nivel: "5",
+        pregunta:
+          "¿Cuáles son los tres predadores del reino animal reconocidos por: 1) habilidad de cazar en grupo, 2) camuflajearse para sorprender a su presa, 3) poseer sentidos refinados?",
+        opcion1: "1) León; 2) tiburón blanco; 3) orca",
+        opcion2: "1) Tiburón blanco; 2) elefante; 3) escorpión",
+        opcion3: "1) Cobra; 2) zorro; 3) cocodrilo",
+        opcionCorrecta: "1) Hiena; 2) Oso polar; 3) Lobo gris",
+      },
+    },
+    {
+      pregunta: {
+        categoria: "historia",
+        nivel: "5",
+        pregunta:
+          "Según la leyenda de la fundación de Roma, ¿quién amamantó a los gemelos Rómulo y Remo?",
+        opcion1: "Una cabra",
+        opcion2: "Una vaca",
+        opcion3: "Una oveja",
+        opcionCorrecta: "Una loba",
+      },
+    },
+    {
+      pregunta: {
+        categoria: "geografia",
+        nivel: "5",
+        pregunta: "¿En qué océano queda Madagascar?",
+        opcion1: "Océano Pacífico",
+        opcion2: "Océano Pacífico",
+        opcion3: "Océano Antártico",
+        opcionCorrecta: "Océano Índico",
+      },
+    },
+    {
+      pregunta: {
+        categoria: "historia",
+        nivel: "5",
+        pregunta: "¿Dónde se originaron los números arábigos?",
+        opcion1: "Arabia",
+        opcion2: "Persia",
+        opcion3: "Roma",
+        opcionCorrecta: "India",
+      },
+    },
+  ],
+
+  historico: [
+    {
+      nickname: "maria",
+      totalAcumulado: 300,
+    },
+    {
+      nickname: "juan",
+      totalAcumulado: 400,
+    },
+  ],
+
+  preguntaActual: 0,
+  acumulado: 100,
+};
